ddab0435676aba577640908c7832113c2a0dea8d
stelioslep <stelios.lep@outlook.com>
2023-04-29 01:44:21 +0300

    Cleaned up presentation of visitor details.

--- app/app.py
+++ app/app.py
@@ -8,8 +8,20 @@ access_token = os.environ['IPINFO_TOKEN']
 
 @app.route('/')
 def index():
+    
     visitor_details=locate_ip(remove_port(get_client_ip()))
-    return render_template('index.html', visitor_details=visitor_details)
+
+    if location_detail_validator(visitor_details):
+        city, region, country, countryname, latitude, longitude = location_detail_extractor(visitor_details)
+    else:
+        city = 'Unknown city'
+        region = 'Unknown region'
+        country = 'Unknown country'
+        countryname = 'Unknown country name'
+        latitude = 'Unknown latitude'
+        longitude = 'Unknown longitude'
+    
+    return render_template('index.html', city=city, region=region, country=country, countryname=countryname, latitude=latitude, longitude=longitude)
 
 def get_client_ip():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
@@ -24,11 +36,25 @@ def remove_port(ip_address_with_port):
     ip_address = parts[0]
     return ip_address
 
-
 def locate_ip(client_ip):
     handler = ipinfo.getHandler(access_token)
     details = handler.getDetails(client_ip)
     return details.all
+
+def location_detail_extractor(detail_dict):
+    city = detail_dict['city']
+    region = detail_dict['region']
+    country = detail_dict['country']
+    countryname = detail_dict['country_name']
+    latitude = detail_dict['latitude']
+    longitude = detail_dict['longitude']
+    return city, region, country, countryname, latitude, longitude
+
+def location_detail_validator(detail_dict):
+    if 'bogon' in detail_dict:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     app.run(debug=True)
