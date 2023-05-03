301e67ab5fce5c8f10f27660aa0ec6d8d8bbf9b5
stelioslep <stelios.lep@outlook.com>
2023-05-03 04:29:45 +0300

    More visual changes.

--- app/app.py
+++ app/app.py
@@ -4,9 +4,6 @@ import os
 
 # check which are necessary
 from pyowm import OWM
-from pyowm.utils import config
-from pyowm.utils import timestamps
-
 
 app = Flask(__name__)
 
@@ -23,16 +20,27 @@ def index():
     if location_detail_validator(visitor_details):
         city, region, country, countryname, latitude, longitude = location_detail_extractor(visitor_details)
     else:
-        city = 'Unknown city'
-        region = 'Unknown region'
-        country = 'Unknown country'
-        countryname = 'Unknown country name'
+        city = 'Munich'
+        region = 'Bavaria'
+        country = 'Germany'
+        countryname = 'DE'
         latitude = 'Unknown latitude'
         longitude = 'Unknown longitude'
     
-    weather_status, humidity, temp_info = weather_search(region, country)
+    #add try block here, check for city, country first then region, country
     
-    return render_template('index.html', city=city, region=region, country=country, countryname=countryname, latitude=latitude, longitude=longitude, weather_status=weather_status, humidity=humidity, temp_info=temp_info)
+    try:
+        weather_status, humidity, now_temperature, max_temperature, min_temperature  = weather_search(city, country)
+        location = city
+    except:
+        weather_status, humidity, now_temperature, max_temperature, min_temperature = weather_search(region, country)
+        location = region
+
+    now_temperature = round(now_temperature)
+    max_temperature = round(max_temperature)
+    min_temperature = round(min_temperature)
+    
+    return render_template('index.html', location=location, country=country,  weather_status=weather_status, humidity=humidity, now_temperature=now_temperature, max_temperature=max_temperature, min_temperature=min_temperature, latitude=latitude, longitude=longitude)
 
 def get_client_ip():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
@@ -67,10 +75,8 @@ def location_detail_validator(detail_dict):
     else:
         return True
 
-# fails locally, works on azure. Add error handling
-
-def weather_search(region, country):
-    observation = mgr.weather_at_place(f'{region},{country}')
+def weather_search(location, country):
+    observation = mgr.weather_at_place(f'{location},{country}')
     w = observation.weather
 
     weather_status = w.detailed_status         # 'clouds'
@@ -80,10 +86,12 @@ def weather_search(region, country):
     rain = w.rain                    # {}
     #w.heat_index              # None
     #w.clouds                  # 75
-    humidity = f"Humidity: {humidity}"
-    temp_info = f"Current temperature: {temperature['temp']}, Max temperature: {temperature['temp_max']}, Min temperature: {temperature['temp_min']}"
+    humidity = humidity
+    now_temperature = temperature['temp'] 
+    max_temperature = temperature['temp_max']
+    min_temperature = temperature['temp_min']
 
-    return weather_status, humidity, temp_info
+    return weather_status, humidity, now_temperature, max_temperature, min_temperature
 
 if __name__ == '__main__':
     app.run(debug=True)
